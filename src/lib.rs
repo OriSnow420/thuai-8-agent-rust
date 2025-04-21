d66d0fef842930f66f1b5a062ba68dd488a0360a
OriSnow420 <1315376634@qq.com>
2025-04-21 22:42:57 +0800

    chore: parse option

---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+
+use tracing::info;
+
+pub async fn run_agent(server: String, token: String) {
+    info!("Connecting to {server} with token {token}");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use thuai_8_agent_rust::run_agent;
+use std::env;
+
+#[derive(Parser)]
+#[command(about, long_about = None)]
+struct Cli {
+    #[arg(long)]
+    server: Option<String>,
+    #[arg(long)]
+    token: Option<String>,
+}
+
+const SERVER_DEFAULT: &str = "127.0.0.1:14514";
+const TOKEN_DEFAULT: &str = "1919810";
+
+#[tokio::main]
+async fn main() {
+    let cli = Cli::parse(); // Read Cli Options
+    tracing_subscriber::fmt::init();
+
+    // Cli goes first, or get the env.
+    let server = cli.server.unwrap_or(
+        env::var("SERVER").unwrap_or(SERVER_DEFAULT.to_string())
+    );
+    let token = cli.token.unwrap_or(
+        env::var("TOKEN").unwrap_or(TOKEN_DEFAULT.to_string())
+    );
+
+    run_agent(server, token).await;
+
 }
